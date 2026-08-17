6358bc81197a9ec0b65174939073cadb4b5aa7db
Baca <jabaca@uamv.edu.ni>
2026-08-17 10:28:48 -0600

    primer commit

---
+++ Ejemplo_1.py
@@ -0,0 +1,24 @@
+import time
+
+print("Bienvenido al segundo semestre")
+print("Mi nombre es","José Baca")
+
+
+#Variables 
+edificio = 13
+precio = 15.5
+print("Edificio #: ", edificio)
+print("Precio: ", precio)
+
+
+lista_de_aura = ["67","Tung Tung Sahur","Aura"]
+
+for memes in lista_de_aura: 
+    print (memes)
+
+for i in range(len(lista_de_aura)):
+    print(f"Indice: {i+1}, Valor: {lista_de_aura[i]}")
+
+for i in range(67):
+    print("")
+    time.sleep(0.67)
