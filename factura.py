e8fd854febd498e4451865897ffa375122abc497
HUMBERTO <hrruiz@uamv.edu.ni>
2026-08-17 10:55:40 -0600

    Agregado los ejericicos 3 y 4

---
+++ factura.py
@@ -0,0 +1,7 @@
+
+n = input("Ingresa el nombre del producto: ")
+p = float(input("Ingresa el precio del producto: "))
+c = float(input("Ingresa la cantidad a llevar: "))
+
+sb = p * c
+print(f"El subtotal del producto {n} es: {sb}")
---
+++ temperatura.py
@@ -0,0 +1,4 @@
+
+gc = float(input("Ingresa una temperatura en grados Celcius: "))
+gf = (gc * 9 / 5) + 32
+print(f"La conversion a grados Fahrenheit es: {gf}")
